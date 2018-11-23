358487a59e8567d68195489eb0dc86c9fc30fc90
Victor Basumatary <victor@intown-solutions.com>
2018-11-23 17:41:16 +0530

    Added extra fries emoji to title text

--- components/title/index.js
+++ components/title/index.js
@@ -6,14 +6,18 @@ import media from '../../lib/media';
 const H1 = styled.h1`
   font-family: 'Pacifico', cursive;
   font-weight: normal;
-  font-size: 44px;
+  font-size: 40px;
   padding-top: 0.5em;
+
+  ${media.tablet`
+    font-size: 44px;
+  `}
 
   ${media.desktop`
     font-size: 48px;
   `}
 `;
 
-const Title = () => <H1>🍟 Good Fries</H1>;
+const Title = () => <H1>🍟 Good Fries 🍟</H1>;
 
 export default Title;
