9214208ad7cab081dddef68ddd90e831ddd808e8
Victor Basumatary <victor@intown-solutions.com>
2018-11-23 09:27:40 +0530

    1. Updated now config 2. Added instructions to home screen

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,24 @@
-export default () => <h1>How to make *le fries</h1>
+import React from 'react';
+
+export default () => (
+  <main style={{padding: '0px 8px'}}>
+    <h1>Good Fries 🍟</h1>
+
+    <ol style={{fontStyle: 'italic'}}>
+      <li>Provision potatoes. Duh.</li>
+      <li>Wash potatoes. Duh.</li>
+      <li>Peel potato skin. Duh.</li>
+      <li>Slice potatoes into fingers. Duh.</li>
+      <li>Heat (lots of) oil in pan. Duh.</li>
+      <li>Fry potato fingers until crispy yellow. Duh.</li>
+      <li>Pull them out and let them rest. Duh.</li>
+      <li>Deep fry em' again for *le crunch&trade;. Duh.</li>
+      <li>Add seasoning. Salt and pepper for the faint hearted.</li>
+      <li>Serve it up.</li>
+    </ol>
+
+    <footer>
+      Made by <a href="https://about.me/vlence" target="_blank" rel="noreferrer noopener">Victor Basumatary</a>
+    </footer>
+  </main>
+);
